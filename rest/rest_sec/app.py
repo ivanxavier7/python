ac236114098416f8513734d0eef1e66c047eb0e2
Ivan Xavier <ivanxavier123@gmail.com>
2021-12-10 12:30:01 +0000

    GET, POST, DELETE in sqlite3
    GET, POST, DELETE in sqlite3

--- rest/rest_sec/app.py
+++ rest/rest_sec/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from user import UserRegister
+from item import Item, ItemList
 
 from security import authenticate, identity
 
@@ -11,56 +12,10 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # Creane new endpoit /auth, send user and password with authenticate, if match uses de JWT token in identity to search the user that is token represents
 
-items = []
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        'price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-        )   # Just accept price, other arguments will be ignored
-    
-    @jwt_required()   # Decorator that require de authentication before "get"
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)     # Search for the first 'name' inside items, if not find retreive None
-        return {'item': item}, 200 if item else 404      # SUCCESS or NOT FOUND
-    
-    def post(self, name):
-        
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400  # BAD REQUEST
-        data = Item.parser.parse_args()
-        item = {
-            'name': name,
-            'price': data['price']
-            }
-        items.append(item)
-        return item, 201                # CREATED
-    
-    def delete(self, name):
-        global items    # To use items variable out of class
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted.'}
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            item.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return{'items': items}
-
 api.add_resource(Item, '/item/<string:name>') # https://127.0.0.1:5000/item/Exemplo
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 
-app.run(port=5000, debug=True)  # debug=True para desenvolver
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)  # debug=True para desenvolver
---
+++ rest/rest_sec/item.py
@@ -0,0 +1,76 @@
+import sqlite3
+from flask import Flask
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+        )   # Just accept price, other arguments will be ignored
+    
+    @jwt_required()   # Decorator that require de authentication before "get"
+    def get(self, name):
+        item =self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+    
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
+    
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400  # BAD REQUEST
+        
+        data = Item.parser.parse_args()
+        item = {
+            'name': name,
+            'price': data['price']
+            }
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+        
+        connection.commit()
+        connection.close()
+        return item, 201                # CREATED
+    
+    def delete(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name, ))
+        
+        connection.commit()
+        connection.close()
+        return {'message': 'Item deleted.'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            item.append(item)
+        else:
+            item.update(data)
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        return{'items': items}
